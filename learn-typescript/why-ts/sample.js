3320c92f21deb512c6ee9d70b9552bdf7710d61a
awjae <djwotmd@gmail.com>
2021-01-07 23:53:54 +0900

    using js like ts

--- learn-typescript/why-ts/sample.js
+++ learn-typescript/why-ts/sample.js
@@ -1,6 +1,21 @@
-function sum(a, b) {
-    return a + b;
-}
+// function sum(a, b) {
+//     return a + b;
+// }
 // sum(10, '20'); // 1020
 //var result = sum(10, 20);
 
+// @ts-check
+
+//JSDoc
+//JSDoc로 VSCode가 preview를 제공함
+/**
+ * 
+ * @param {number} a 첫번째 숫자
+ * @param {number} b 두번째 숫자
+ */
+function sum(a, b) {
+    return a + b;
+}
+sum(10, 20);
+sum(10, '20'); // 하지만 에러를 표출해주지않는다 
+// js 로 타입추론이 가능하긴하지만 역시 ts가 더 강력하다
