8a4a9cfabeba3c0f04b6368c2dc98e326fc00b26
awjae <djwotmd@gmail.com>
2021-01-17 20:19:39 +0900

    enum ans operator

--- learn-typescript/class-node/6_enum.ts
+++ learn-typescript/class-node/6_enum.ts
@@ -13,14 +13,19 @@ var myShoes = Shoes.Nike;
 console.log(myShoes)
 
 // 예제
-function askQuestion(answer: string) {
-    if (answer === 'yes') {
+enum Answer {
+    Yes = 'Y',
+    No = 'N'
+}
+
+
+function askQuestion(answer: Answer) {
+    if (answer === Answer.Yes) {
         console.log('정답입니다')
     }
-    if (answer === 'no') {
+    if (answer === Answer.No) {
         console.log('오답입니다.')
     }
 }
-askQuestion('예스');
-askQuestion('y');
-askQuestion('Yes');
+askQuestion(Answer.Yes);
+askQuestion(Answer.No);
